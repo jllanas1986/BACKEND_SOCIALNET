c90e0f59f9165843226b4b6e09ca8c7a0c7a6fea
joanbaldo <joanbaldo@hotmail.com>
2023-04-21 00:01:40 +0200

    Post-UPDATE ,created ROUTE

--- routes/posts.js
+++ routes/posts.js
@@ -3,8 +3,8 @@ const express = require('express');
 const router = express.Router()
 const PostController = require('../controllers/PostController');
 
-router.post('/create',PostController.create) //RUTA: Endpoint CREAR producto
-
+router.post('/create',PostController.create) //ROUTE: Endpoint CREATE post
+router.put('/update/:_id', PostController.update) // ROUTE: Endpoint UPDATE post
 
 
 
