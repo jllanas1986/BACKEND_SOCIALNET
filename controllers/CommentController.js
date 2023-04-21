a1c684b63bdb9f3969d8d294bf93c04308c17db8
joanbaldo <joanbaldo@hotmail.com>
2023-04-21 08:28:50 +0200

    Comment-CREATE ,created CONTROLLER

---
+++ controllers/CommentController.js
@@ -0,0 +1,19 @@
+// FALTA TESTEAR 
+const Comment = require("../models/Comment");
+
+const CommentController = {
+
+    //ENDPOINT: CREATE COMMENT
+    async create(req, res) {
+        try {
+            const comment = await Comment.create(req.body)
+            res.status(201).send(comment)
+        } catch (error) {
+            console.error(error)
+            res.status(500).send({ message: 'Ha habido un problema al crear el comentario' })
+        }
+
+    },
+}
+
+module.exports = CommentController;
