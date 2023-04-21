01fc12be1efe6909ebbebbe9fb5ba7c85aa84f09
joanbaldo <joanbaldo@hotmail.com>
2023-04-21 12:29:56 +0200

    Relations created on POST and COMMENT models

--- models/Comment.js
+++ models/Comment.js
@@ -1,10 +1,22 @@
-// FALTA TESTEAR 
 const mongoose = require('mongoose');
+const ObjectId = mongoose.SchemaTypes.ObjectId;
+
+
 const CommentSchema = new mongoose.Schema({
-user: String, // Sería user_id ?
+
 post: String, // Sería post_id ?
 content: String, //Para no usar "body" como en "Post"
+// Relacionamos USER--> Comments
+userId: {
+    type: ObjectId,
+    ref: 'User'
+    },
+// Relacionamos POST--> Comments
+postId: {
+        type: ObjectId,
+        ref: 'Post'
+        },
 
 }, { timestamps: true });
-const Comment = mongoose.model('Comment', PostSchema);
+const Comment = mongoose.model('Comment', CommentSchema);
 module.exports = Comment;
