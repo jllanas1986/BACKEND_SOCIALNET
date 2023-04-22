20be090956de8112a1b260bca9e4a59d57266257
Jose Llanas <josellanasmota86@gmail.com>
2023-04-22 18:22:05 +0200

    bycript implemented

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,9 +1,16 @@
 const User = require("../models/User");
+const bcrypt = require ('bcryptjs');
 
 const UserController = {
   async register(req, res) {
+    req.body.role = "user";
+    const password = req.body.password;
+    let hashedPassword;
+    if(password) {
+      hashedPassword = bcrypt.hashSync(password, 10) ///encriptando clave de acceso
+    };
     try {
-      const user = await User.create(req.body);
+      const user = await User.create({...req.body, password: hashedPassword});
       res.status(201).send({ message: "Usuario registrado con exito", user });
     } catch (error) {
       console.error(error);
