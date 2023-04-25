4ea0abdba6b6bb790d609363289fc770b23de466
joanbaldo <joanbaldo@hotmail.com>
2023-04-25 19:30:32 +0200

    COOMENTS, created a new endpoint to create comment based on Post ID - NOT WORKING

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -18,8 +18,26 @@ const CommentController = {
             console.error(error)
             res.status(500).send({ message: 'Ha habido un problema al crear el comentario' })
         }
-
     },
+
+    //ENDPOINT: CREATE COMMENT (V.2)
+    
+    async createComment(req, res) {
+    try {
+    const post = await Post.findByIdAndUpdate(
+    req.params._id,
+    { $push: { commentIds: { commentTitle:req.body.commentTitle,commentBody: req.body.commentBody, userId: req.user._id } }
+    },
+    { new: true }
+    );
+    res.send(post);
+    } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: "There was a problem with your comment" });
+    }
+    },    
+
+
     //ENDPOINT: DELETE COMMENT (requiere validar autoría)
 
     async delete(req, res) {
