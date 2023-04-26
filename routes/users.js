2ce3119b5fa5e2bf6dbd7992a34614c0025dd44c
Jose Llanas <josellanasmota86@gmail.com>
2023-04-26 16:04:19 +0200

    authentication reestablished in endpoint user info

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ router.post('/register',UserController.register)
 router.post('/login',UserController.login)
 router.delete('/logout',authentication,UserController.logout)
 router.delete('/deleteUserById/:_id',authentication, isAdmin, UserController.delete) //Añadido validación como ADMIN para borrar.
-router.get('/info', UserController.getInfo)
+router.get('/info', authentication, UserController.getInfo)
 router.get('/getById/:_id',UserController.getById)
 router.get('/getByIndex/:name',UserController.getUsersByName)
 
