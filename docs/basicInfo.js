6561d6ebe096b09011722300a7477ca633fe19ec
joanbaldo <joanbaldo@hotmail.com>
2023-04-28 17:06:16 +0200

    SWAGGER initial setup

---
+++ docs/basicInfo.js
@@ -0,0 +1,8 @@
+module.exports = {
+    openapi:"3.0.3",
+    info:{
+    version:"1.0.2",
+    title:"THESOCIALAPI",
+    description: "La API que te ayuda a construir redes sociales"
+    }
+    }
