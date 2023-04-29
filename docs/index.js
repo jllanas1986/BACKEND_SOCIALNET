69f4a6c34096ee22882c5e7235ec50bb43cf5288
joanbaldo <joanbaldo@hotmail.com>
2023-04-29 21:31:45 +0200

    SWAGGER index.js updated

--- docs/index.js
+++ docs/index.js
@@ -1,7 +1,10 @@
 const basicInfo = require('./basicInfo');
 const components = require('./components');
+const users = require('./users');
+
 
 module.exports = {
     ...basicInfo,
-    ...components
+    ...components,
+    ...users
 };
