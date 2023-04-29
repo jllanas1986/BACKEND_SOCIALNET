f9bad135b867489da25e08f262479fb256fe822f
joanbaldo <joanbaldo@hotmail.com>
2023-04-29 21:37:11 +0200

    MAIN  index.js updated

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ app.use(express.json());
 
 dbConnection();
 
+app.use('/api-docs', swaggerUI.serve,swaggerUI.setup(docs)) 
+
+
 // RUTAS IMPORTADAS
 app.use('/posts', require('./routes/posts'));
 app.use('/comments', require('./routes/comments'));
@@ -23,7 +26,7 @@ app.use(handleTypeError)
 //AHORA LAS RUTAS PARA SWAGGER
 // app.use("/tasks", require("./routes/tasks"));
 
-app.use('/api-docs', swaggerUI.serve,swaggerUI.setup(docs)) // La ruta hacia la DOCUMENTACIÓN DE LA API! OJO
+//app.use('/api-docs', swaggerUI.serve,swaggerUI.setup(docs)) // La ruta hacia la DOCUMENTACIÓN DE LA API! OJO
 //El (docs) de arriba (linea 26) refiere al de la linea 8. Todo lo que contenga ese doscs lo mostraremos en el endpoint de la documentación ES DECIR...contendrá todo lo que el docs> index.js contenga.
 
 
